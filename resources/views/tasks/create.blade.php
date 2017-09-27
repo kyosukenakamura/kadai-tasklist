56ba5812dde52a8881ac94b0fce6f4c43ba46069
nakada.nakada <nakamurato830@gmail.com>
2017-09-27 11:47:41 +0000

    CRUD tasklist

---
+++ resources/views/tasks/create.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.app')
+
+@section('content')
+
+    <h1>新規タスクの作成</h1>
+    
+    {!! Form::model($task, ['route' => 'tasks.store']) !!}
+    
+        {!! Form::label('content', 'タスク') !!}
+        {!! Form::text('content') !!}
+        
+        {!! Form::submit('投稿') !!}
+    
+    {!! Form::close() !!}
+
+@endsection
