f0470730123c531daf4de4039b49e7358f5a642e
nakada.nakada <nakamurato830@gmail.com>
2017-09-28 08:28:42 +0000

    bootstrap design

--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -4,13 +4,29 @@
 
     <h1>新規タスクの作成</h1>
     
-    {!! Form::model($task, ['route' => 'tasks.store']) !!}
+    <div class="row">
+        <div class="col-xs-12 
+                    col-sm-8 col-sm-offset-2
+                    col-md-8 col-md-offset-2
+                    col-lg-6 col-lg-offset-3">
+            
+            {!! Form::model($task, ['route' => 'tasks.store']) !!}
     
-        {!! Form::label('content', 'タスク') !!}
-        {!! Form::text('content') !!}
-        
-        {!! Form::submit('投稿') !!}
+                <div class="form-group">
+                    {!! Form::label('status', 'ステータス:') !!}
+                    {!! Form::text('status', null, ['class' => 'form-control']) !!}
+                </div>
+                
+                <div class="form-group">
+                    {!! Form::label('content', 'タスク:') !!}
+                    {!! Form::text('content', null, ['class' => 'form-control']) !!}
+                </div>
+                
+                    {!! Form::submit('投稿', ['class' => 'btn btn-primary']) !!}
+            
+            {!! Form::close() !!}
+            
+        </div>
+    </div>
     
-    {!! Form::close() !!}
-
 @endsection
